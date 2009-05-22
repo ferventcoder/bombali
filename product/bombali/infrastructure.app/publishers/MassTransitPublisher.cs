99ef8471a013891876dd7ad708c0d9264114e6a2
ferventcoder <ferventcoder@bc8673c8-3b5b-11de-a5d8-8da2cac0bf50>
2009-05-22 16:37:04 +0000

    Updating the app.config to use Cyan for debug messages. adding a publishers group to use for notifications instead.

---
+++ product/bombali/infrastructure.app/publishers/MassTransitPublisher.cs
@@ -0,0 +1,10 @@
+namespace bombali.infrastructure.app.publishers
+{
+	class MassTransitPublisher : IPublisher
+	{
+		public void publish(string message)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
--- product/bombali/runners/BombaliServiceRunner.cs
+++ product/bombali/runners/BombaliServiceRunner.cs
@@ -1,36 +1,35 @@
 namespace bombali.runners
 {
-    using System.Collections.Generic;
-    using System.Threading;
-    using domain;
-    using orm;
+	using System.Collections.Generic;
+	using domain;
+	using orm;
 
-    public class BombaliServiceRunner : IRunner
-    {
-        readonly IList<IPersistenceStore> persistence_stores;
+	public class BombaliServiceRunner : IRunner
+	{
+		private readonly IList<IPersistenceStore> persistence_stores;
 
-        public BombaliServiceRunner(IEnumerable<IPersistenceStore> persistence_stores)
-        {
-            this.persistence_stores = new List<IPersistenceStore>(persistence_stores);
-        }
+		public BombaliServiceRunner(IEnumerable<IPersistenceStore> persistence_stores)
+		{
+			this.persistence_stores = new List<IPersistenceStore>(persistence_stores);
+		}
 
-        public void run_the_application()
-        {
-            IList<IMonitor> monitors = new List<IMonitor>();
+		public void run_the_application()
+		{
+			IList<IMonitor> monitors = new List<IMonitor>();
 
-            foreach(IPersistenceStore persistence_store in persistence_stores)
-            {
-                foreach(IMonitor monitor in persistence_store.map_to_monitors()) monitors.Add(monitor);
-            }
+			foreach (IPersistenceStore persistence_store in persistence_stores)
+			{
+				foreach (IMonitor monitor in persistence_store.map_to_monitors()) monitors.Add(monitor);
+			}
 
-            foreach(IMonitor monitor in monitors)
-            {
-                new Thread(() => 
-                    { 
-                        if(monitor != null) monitor.start_monitoring(); 
-                    }
-                    ).Start();
-            }
-        }
-    }
+			foreach (IMonitor monitor in monitors)
+			{
+				//new Thread(() => 
+				//    { 
+				if (monitor != null) monitor.start_monitoring();
+				//}
+				//).Start();
+			}
+		}
+	}
 }
