ccefc2038a009855bd437b855d9aa666ef5bc027
ferventcoder <ferventcoder@bc8673c8-3b5b-11de-a5d8-8da2cac0bf50>
2009-07-25 12:08:33 +0000

    upgrading UppercuT to version 122

--- test.bat
+++ test.bat
@@ -14,8 +14,16 @@ SET DIR=%~d0%~p0%
 
 SET build.config.settings="%DIR%Settings\UppercuT.config"
 "%DIR%lib\tools\Nant\nant.exe" /f:.\BuildScripts\_compile.build -D:build.config.settings=%build.config.settings%
+
+if %ERRORLEVEL% NEQ 0 goto errors
+
 "%DIR%lib\tools\Nant\nant.exe" /f:.\BuildScripts\analyzers\_test.build %1 -D:build.config.settings=%build.config.settings%
+
+if %ERRORLEVEL% NEQ 0 goto errors
+
 "%DIR%lib\tools\Nant\nant.exe" /f:.\BuildScripts\analyzers\_test.build open_results -D:build.config.settings=%build.config.settings%
+
+if %ERRORLEVEL% NEQ 0 goto errors
 
 goto finish
 
@@ -25,5 +33,8 @@ echo Usage: test.bat
 echo Usage: test.bat all - to run all tests
 echo.
 goto finish
+
+:errors
+EXIT /B %ERRORLEVEL%
 
 :finish
