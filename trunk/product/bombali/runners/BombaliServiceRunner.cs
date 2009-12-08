fac137c3dc444839c9692736e35aabbc58735fbb
ferventcoder <ferventcoder@bc8673c8-3b5b-11de-a5d8-8da2cac0bf50>
2009-12-08 04:18:59 +0000

    adding sidePOP to the configuration. This allows you to interact with Bombali. Right now it's used to ensure that Bombali can reach you and is still running

--- trunk/product/bombali/runners/BombaliServiceRunner.cs
+++ trunk/product/bombali/runners/BombaliServiceRunner.cs
@@ -1,35 +1,75 @@
 namespace bombali.runners
 {
-	using System.Collections.Generic;
-	using domain;
-	using orm;
+    using System.Collections.Generic;
+    using domain;
+    using infrastructure;
+    using infrastructure.app.settings;
+    using infrastructure.logging;
+    using infrastructure.notifications;
+    using orm;
+    using sidepop.configuration;
+    using sidepop.Mail;
+    using sidepop.message.events;
+    using sidepop.runners;
 
-	public class BombaliServiceRunner : IRunner
-	{
-		private readonly IList<IPersistenceStore> persistence_stores;
+    public class BombaliServiceRunner : IRunner
+    {
+        private readonly IList<IPersistenceStore> persistence_stores;
 
-		public BombaliServiceRunner(IEnumerable<IPersistenceStore> persistence_stores)
-		{
-			this.persistence_stores = new List<IPersistenceStore>(persistence_stores);
-		}
+        public BombaliServiceRunner(IEnumerable<IPersistenceStore> persistence_stores)
+        {
+            this.persistence_stores = new List<IPersistenceStore>(persistence_stores);
+        }
 
-		public void run_the_application()
-		{
-			IList<IMonitor> monitors = new List<IMonitor>();
+        public void run_the_application()
+        {
+            configure_mail_watcher();
+            start_monitoring();
+        }
 
-			foreach (IPersistenceStore persistence_store in persistence_stores)
-			{
-				foreach (IMonitor monitor in persistence_store.map_to_monitors()) monitors.Add(monitor);
-			}
+        private void configure_mail_watcher()
+        {
+            foreach (AccountConfigurationElement account in SidePOPConfiguration.settings.accounts)
+            {
+                if (account.enabled)
+                {
+                    SidePopRunner runner = new SidePopRunner(new DefaultPop3Client(account.hostName, account.hostPort,
+                                                                            account.useSSL, account.userName,
+                                                                            account.password), account.minutes_between_checks);
+                    runner.MessagesReceived += runner_messages_received;
+                    runner.run();
+                }
+            }
+        }
 
-			foreach (IMonitor monitor in monitors)
-			{
-				//new Thread(() => 
-				//    { 
-				if (monitor != null) monitor.start_monitoring();
-				//}
-				//).Start();
-			}
-		}
-	}
+        private void runner_messages_received(object sender, MessageListEventArgs e)
+        {
+            IEnumerable<MailMessageExtended> messages = e.Messages;
+            foreach (MailMessageExtended message in messages)
+            {
+                Log.bound_to(this).Info("{0} received a message from {1}. Responding that service is still running.",ApplicationParameters.name,message.From.Address);
+                SendNotification.from(BombaliConfiguration.settings.email_from).to(message.From.Address).with_subject(
+                "Bombali Received Message").with_message("Bombali is up and running.").and_use_notification_host(BombaliConfiguration.settings.smtp_host);
+            }
+        }
+
+        private void start_monitoring()
+        {
+            IList<IMonitor> monitors = new List<IMonitor>();
+
+            foreach (IPersistenceStore persistence_store in persistence_stores)
+            {
+                foreach (IMonitor monitor in persistence_store.map_to_monitors()) monitors.Add(monitor);
+            }
+
+            foreach (IMonitor monitor in monitors)
+            {
+                //new Thread(() => 
+                //    { 
+                if (monitor != null) monitor.start_monitoring();
+                //}
+                //).Start();
+            }
+        }
+    }
 }
