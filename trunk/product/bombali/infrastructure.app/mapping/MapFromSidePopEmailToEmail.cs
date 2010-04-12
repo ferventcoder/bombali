101dcee8a95ed7cabc99cb8c5f260f917d65d9e1
ferventcoder <ferventcoder@bc8673c8-3b5b-11de-a5d8-8da2cac0bf50>
2010-04-12 18:39:19 +0000

    now able to handle phones that send multimedia messages

--- trunk/product/bombali/infrastructure.app/mapping/MapFromSidePopEmailToEmail.cs
+++ trunk/product/bombali/infrastructure.app/mapping/MapFromSidePopEmailToEmail.cs
@@ -1,5 +1,6 @@
 namespace bombali.infrastructure.app.mapping
 {
+    using System;
     using System.Net.Mail;
     using System.Text;
     using domain;
@@ -12,7 +13,7 @@ namespace bombali.infrastructure.app.mapping
     {
         public Email map_from(SidePOPMailMessage from)
         {
-            Log.bound_to(this).Debug("{0} is mapping message \"{1}\" to a domain type email message for processing and archival.",ApplicationParameters.name,from.MessageId);
+            Log.bound_to(this).Debug("{0} is mapping message \"{1}\" to a domain type email message for processing and archival.", ApplicationParameters.name, from.MessageId);
             Email email = new Email
                               {
                                   // message metadata
@@ -28,7 +29,38 @@ namespace bombali.infrastructure.app.mapping
                                   subject = from.Subject,
                                   message_body = from.Body
                               };
-            
+
+            try
+            {
+                //this is for multimedia messages that come from some phones - I'm looking at you AT&T
+                if (string.IsNullOrEmpty(email.message_body))
+                {
+                    Log.bound_to(this).Info("Message Body is null - looking in attachments for message.");
+
+                    if (from.Attachments != null && from.Attachments.Count >= 1)
+                    {
+                        Log.bound_to(this).Debug("There is at least one attachment.");
+                        foreach (Attachment attachment in from.Attachments)
+                        {
+                            Log.bound_to(this).Debug("Content Type MediaType|CharSet = {0}.", attachment.ContentType != null ? attachment.ContentType.MediaType + "|" + attachment.ContentType.CharSet : string.Empty);
+                            if (attachment.ContentType != null && attachment.ContentType.MediaType.to_lower().Contains("text"))
+                            {
+                                byte[] buffer = new byte[attachment.ContentStream.Length];
+                                attachment.ContentStream.Read(buffer, 0, buffer.Length);
+                                email.message_body = Encoding.UTF8.GetString(buffer);
+                                Log.bound_to(this).Info("After conversion - this is the message body:{0}{1}", Environment.NewLine, email.message_body);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.bound_to(this).Error("{0} service had an error getting message body from attachment on {1} (with user {2}):{3}{4}", ApplicationParameters.name,
+                                    Environment.MachineName, Environment.UserName,
+                                    Environment.NewLine, ex.ToString());
+            }
+
 
             return email;
         }
